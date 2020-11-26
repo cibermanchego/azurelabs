dab830e9e8c299c28a26b68b1f990dc6f39c69c7
cibermanchego <ainigom@gmail.com>
2020-11-26 19:43:41 +0100

    more cleanup

--- provider.tf
+++
@@ -1,7 +0,0 @@
-provider "azurerm" {
-  # The version is constantly evolving, make sure to check https://github.com/terraform-providers/terraform-provider-azurerm/releases
-  #version = "=2.12.0"
-  version = "=2.37.0"
-  features {}
-}   
-
