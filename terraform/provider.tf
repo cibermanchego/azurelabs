4e523bcb2718b278f9bce6707aa56c37edf519cc
cibermanchego <ainigom@gmail.com>
2020-12-01 19:25:36 +0100

    Clean provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,8 +2,6 @@ provider "azurerm" {
   # The version is constantly evolving, make sure to check https://github.com/terraform-providers/terraform-provider-azurerm/releases
 #  version = "=2.12.0"
   version = "=2.38.0"
-  subscription_id ="3ed1bd30-7f5d-4de7-a59f-609afa2d1c98"
-  tenant_id =  "48473335-3fe5-4b62-8e3d-9d02d1931a66"
   features {}
 }   
 
