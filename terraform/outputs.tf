8630a14b560120c2a8b1d02a1d4fbf5dd2967174
cibermanchego <ainigom@gmail.com>
2020-12-14 13:49:24 +0100

    cleaning

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,18 +1,18 @@
-output "dc_public_ip" {
-  value = azurerm_public_ip.dc_public_ip.ip_address
-}
+# output "dc_public_ip" {
+#   value = azurerm_public_ip.dc_public_ip.ip_address
+# }
 
 # output "domain_controller_password" {
 #   value = random_password.domain_controller_password.result
 # }
 
 #Output all public IPs of WS created
-output "ws_public_ips" {
-  value = azurerm_public_ip.ws_public_ip.*.ip_address
-}
-output "logger_public_ip" {
-  value = azurerm_public_ip.logger_public_ip.ip_address
-}
+# output "ws_public_ips" {
+#   value = azurerm_public_ip.ws_public_ip.*.ip_address
+# }
+# output "logger_public_ip" {
+#   value = azurerm_public_ip.logger_public_ip.ip_address
+# }
 
 output "wef_public_ip" {
   value = azurerm_public_ip.wef_public_ip.ip_address
@@ -24,22 +24,22 @@ output "wef_public_ip" {
 #   }
 # }
 
-output "what_next" {
-  value = <<EOF
+# output "what_next" {
+#   value = <<EOF
 
-#############################
-###  CREDENTIALS DETAILS  ###
-#############################
+# #############################
+# ###  CREDENTIALS DETAILS  ###
+# #############################
 
-- Domain Admin: domadmin / Itisfine123.
-- Local admin all windows: ${var.windows_local_admin} / ${var.windows_local_admin_password}
-- RDP Domain Controller: cmdkey /generic:"${azurerm_public_ip.dc_public_ip.ip_address}" /user:"${var.windows_local_admin}" /pass:"${var.windows_local_admin_password}"
-                        mstsc /v:${azurerm_public_ip.dc_public_ip.ip_address}
-- Login to logger box: ssh -i ${var.private_key_path} loggeradmin@${azurerm_public_ip.logger_public_ip.ip_address}
+# - Domain Admin: domadmin / Itisfine123.
+# - Local admin all windows: ${var.windows_local_admin} / ${var.windows_local_admin_password}
+# - RDP Domain Controller: cmdkey /generic:"${azurerm_public_ip.dc_public_ip.ip_address}" /user:"${var.windows_local_admin}" /pass:"${var.windows_local_admin_password}"
+#                         mstsc /v:${azurerm_public_ip.dc_public_ip.ip_address}
+# - Login to logger box: ssh -i ${var.private_key_path} loggeradmin@${azurerm_public_ip.logger_public_ip.ip_address}
 
 
-EOF
-}
+# EOF
+# }
 
 # output "logger_username" {
 #   value = var.logger_admin_user
--- terraform/windows.tf
+++ terraform/windows.tf
@@ -2,86 +2,86 @@
 
 #Resources for DC
 
-#Public IP
-resource "azurerm_public_ip" "dc_public_ip" {
-  name                = "dc-public-ip"
-  location            = var.region
-  resource_group_name = azurerm_resource_group.itisfine.name
-  allocation_method   = "Static"
-  tags = {
-    role = "domain-controller"
-  }
-}
-
-#NIC
-resource "azurerm_network_interface" "dc_nic" {
-  name = "dc-nic"
-  location = var.region
-  resource_group_name  = azurerm_resource_group.itisfine.name
-  #IP ADDRESS: 192.168.10.10
-  ip_configuration {
-    name                          = "DC-NicConfiguration"
-    subnet_id                     = azurerm_subnet.itisfine_servers.id
-    private_ip_address_allocation = "Static"
-    private_ip_address            = cidrhost(var.servers_subnet_cidr, 10) # servers_subnet_cidr variable defined at vars.tf
-    public_ip_address_id          = azurerm_public_ip.dc_public_ip.id
-  }
-}
-
-#VM
-#Generate random password 16 chars
-# resource "random_password" "domain_controller_password" {
-#   length = 16
+# #Public IP
+# resource "azurerm_public_ip" "dc_public_ip" {
+#   name                = "dc-public-ip"
+#   location            = var.region
+#   resource_group_name = azurerm_resource_group.itisfine.name
+#   allocation_method   = "Static"
+#   tags = {
+#     role = "domain-controller"
+#   }
 # }
 
-#virtual machine resource
-resource "azurerm_virtual_machine" "domain_controller" {
-  name                  = "domain-controller"
-  location              = azurerm_resource_group.itisfine.location
-  resource_group_name   = azurerm_resource_group.itisfine.name
-  network_interface_ids = [azurerm_network_interface.dc_nic.id]
-  # List of available sizes: https://docs.microsoft.com/en-us/azure/cloud-services/cloud-services-sizes-specs
-  vm_size               = "Standard_D1_v2"
-  # Base image
-  storage_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2019-Datacenter"
-    version   = "latest"
-  }
+# #NIC
+# resource "azurerm_network_interface" "dc_nic" {
+#   name = "dc-nic"
+#   location = var.region
+#   resource_group_name  = azurerm_resource_group.itisfine.name
+#   #IP ADDRESS: 192.168.10.10
+#   ip_configuration {
+#     name                          = "DC-NicConfiguration"
+#     subnet_id                     = azurerm_subnet.itisfine_servers.id
+#     private_ip_address_allocation = "Static"
+#     private_ip_address            = cidrhost(var.servers_subnet_cidr, 10) # servers_subnet_cidr variable defined at vars.tf
+#     public_ip_address_id          = azurerm_public_ip.dc_public_ip.id
+#   }
+# }
+
+# #VM
+# #Generate random password 16 chars
+# # resource "random_password" "domain_controller_password" {
+# #   length = 16
+# # }
+
+# #virtual machine resource
+# resource "azurerm_virtual_machine" "domain_controller" {
+#   name                  = "domain-controller"
+#   location              = azurerm_resource_group.itisfine.location
+#   resource_group_name   = azurerm_resource_group.itisfine.name
+#   network_interface_ids = [azurerm_network_interface.dc_nic.id]
+#   # List of available sizes: https://docs.microsoft.com/en-us/azure/cloud-services/cloud-services-sizes-specs
+#   vm_size               = "Standard_D1_v2"
+#   # Base image
+#   storage_image_reference {
+#     publisher = "MicrosoftWindowsServer"
+#     offer     = "WindowsServer"
+#     sku       = "2019-Datacenter"
+#     version   = "latest"
+#   }
   
-  # Disk
-  # Delete OS disk automatically when deleting the VM
-  delete_os_disk_on_termination = true
-  # Delete data disks automatically when deleting the VM
-  delete_data_disks_on_termination = true
-  storage_os_disk {
-    name              = "domain-controller-os-disk"
-    create_option     = "FromImage"
-    caching           = "ReadWrite"
-    managed_disk_type = "Standard_LRS"
-  }
-  os_profile {
-    computer_name  = "dc1"
-    # Note: you can't use admin or Administrator in here, Azure won't allow you to do so :-)
-    admin_username = var.windows_local_admin
-    #password generated before
-    #admin_password = random_password.domain_controller_password.result
-    #Change to hardcoded password
-    admin_password = var.windows_local_admin_password
-  }
-  #Enable WINRM
-  os_profile_windows_config {
-      enable_automatic_upgrades = false
-      timezone = "Central European Standard Time"
-      winrm {
-        protocol = "HTTP"
-      }
-  }
-  tags = {
-    role = "domain-controller"
-  }
-}
+#   # Disk
+#   # Delete OS disk automatically when deleting the VM
+#   delete_os_disk_on_termination = true
+#   # Delete data disks automatically when deleting the VM
+#   delete_data_disks_on_termination = true
+#   storage_os_disk {
+#     name              = "domain-controller-os-disk"
+#     create_option     = "FromImage"
+#     caching           = "ReadWrite"
+#     managed_disk_type = "Standard_LRS"
+#   }
+#   os_profile {
+#     computer_name  = "dc1"
+#     # Note: you can't use admin or Administrator in here, Azure won't allow you to do so :-)
+#     admin_username = var.windows_local_admin
+#     #password generated before
+#     #admin_password = random_password.domain_controller_password.result
+#     #Change to hardcoded password
+#     admin_password = var.windows_local_admin_password
+#   }
+#   #Enable WINRM
+#   os_profile_windows_config {
+#       enable_automatic_upgrades = false
+#       timezone = "Central European Standard Time"
+#       winrm {
+#         protocol = "HTTP"
+#       }
+#   }
+#   tags = {
+#     role = "domain-controller"
+#   }
+# }
 
 
 # #-------------------------------
@@ -174,84 +174,84 @@ resource "azurerm_virtual_machine" "wef" {
 
 # #Resources for Windows 10 VM
 
-#Public IP 1 per WS
-resource "azurerm_public_ip" "ws_public_ip" {
-  count = var.num_ws #index
-  name                = "ws-${count.index + 1}-public-ip"
-  location            = var.region
-  resource_group_name = azurerm_resource_group.itisfine.name
-  allocation_method   = "Static"
+# #Public IP 1 per WS
+# resource "azurerm_public_ip" "ws_public_ip" {
+#   count = var.num_ws #index
+#   name                = "ws-${count.index + 1}-public-ip"
+#   location            = var.region
+#   resource_group_name = azurerm_resource_group.itisfine.name
+#   allocation_method   = "Static"
 
-  tags = {
-    role = "workstation"
-  }
-}
-
-#NIC 1 per WS
-resource "azurerm_network_interface" "ws_nic" {
-  count = var.num_ws #index
-  name = "ws-${count.index + 1}-nic"
-  location = var.region
-  resource_group_name  = azurerm_resource_group.itisfine.name
-  #IP ADDRESS: 192.168.20.10
-  ip_configuration {
-    name                          = "WS-NicConfiguration"
-    subnet_id                     = azurerm_subnet.itisfine_workstations.id
-    private_ip_address_allocation = "Static"
-    private_ip_address            = cidrhost(var.workstations_subnet_cidr, 10 + count.index) # workstations_subnet_cidr variable defined at vars.tf, host 10 + index
-    public_ip_address_id          = azurerm_public_ip.ws_public_ip[count.index].id #IP by index
-  }
-}
-
-#Generate random password 16 chars
-# resource "random_password" "ws_password" {
-#   length = 16
+#   tags = {
+#     role = "workstation"
+#   }
 # }
 
-#virtual machine resource
-resource "azurerm_virtual_machine" "ws" {
-  count = var.num_ws
-  name                  = "ws${count.index + 1}" #Name as ws + index
-  location              = azurerm_resource_group.itisfine.location
-  resource_group_name   = azurerm_resource_group.itisfine.name
-  network_interface_ids = [azurerm_network_interface.ws_nic[count.index].id] #NIC assign by index
-  # https://docs.microsoft.com/en-us/azure/virtual-machines/sizes-b-series-burstable
-  vm_size               = "Standard_B1s"
-  # Base image VM Gen 2 https://docs.microsoft.com/en-us/azure/virtual-machines/generation-2
-  storage_image_reference {
-    publisher = "MicrosoftWindowsDesktop"
-    offer     = "windows-10"
-    sku       = "19h2-pro-g2"
-    version   = "latest"
-  }
+# #NIC 1 per WS
+# resource "azurerm_network_interface" "ws_nic" {
+#   count = var.num_ws #index
+#   name = "ws-${count.index + 1}-nic"
+#   location = var.region
+#   resource_group_name  = azurerm_resource_group.itisfine.name
+#   #IP ADDRESS: 192.168.20.10
+#   ip_configuration {
+#     name                          = "WS-NicConfiguration"
+#     subnet_id                     = azurerm_subnet.itisfine_workstations.id
+#     private_ip_address_allocation = "Static"
+#     private_ip_address            = cidrhost(var.workstations_subnet_cidr, 10 + count.index) # workstations_subnet_cidr variable defined at vars.tf, host 10 + index
+#     public_ip_address_id          = azurerm_public_ip.ws_public_ip[count.index].id #IP by index
+#   }
+# }
 
-  # Disk
-  # Delete OS disk automatically when deleting the VM
-  delete_os_disk_on_termination = true
-  # Delete data disks automatically when deleting the VM
-  delete_data_disks_on_termination = true
-  storage_os_disk {
-    name              = "ws-${count.index + 1}-os-disk"
-    create_option     = "FromImage"
-    caching           = "ReadWrite"
-    managed_disk_type = "Standard_LRS"
-  }
-  os_profile {
-    computer_name  = "ws${count.index + 1}" #set hostname same as name of VM
-    # Note: you can't use admin or Administrator in here, Azure won't allow you to do so :-)
-    admin_username = var.windows_local_admin
-    #password hardcoded
-    admin_password = var.windows_local_admin_password
-  }
-  #Enable WINRM
-  os_profile_windows_config {
-      enable_automatic_upgrades = false
-      timezone = "Central European Standard Time"
-      winrm {
-        protocol = "HTTP"
-      }
-  }
-  tags = {
-    role = "workstation"
-  }
-}
+# #Generate random password 16 chars
+# # resource "random_password" "ws_password" {
+# #   length = 16
+# # }
+
+# #virtual machine resource
+# resource "azurerm_virtual_machine" "ws" {
+#   count = var.num_ws
+#   name                  = "ws${count.index + 1}" #Name as ws + index
+#   location              = azurerm_resource_group.itisfine.location
+#   resource_group_name   = azurerm_resource_group.itisfine.name
+#   network_interface_ids = [azurerm_network_interface.ws_nic[count.index].id] #NIC assign by index
+#   # https://docs.microsoft.com/en-us/azure/virtual-machines/sizes-b-series-burstable
+#   vm_size               = "Standard_B1s"
+#   # Base image VM Gen 2 https://docs.microsoft.com/en-us/azure/virtual-machines/generation-2
+#   storage_image_reference {
+#     publisher = "MicrosoftWindowsDesktop"
+#     offer     = "windows-10"
+#     sku       = "19h2-pro-g2"
+#     version   = "latest"
+#   }
+
+#   # Disk
+#   # Delete OS disk automatically when deleting the VM
+#   delete_os_disk_on_termination = true
+#   # Delete data disks automatically when deleting the VM
+#   delete_data_disks_on_termination = true
+#   storage_os_disk {
+#     name              = "ws-${count.index + 1}-os-disk"
+#     create_option     = "FromImage"
+#     caching           = "ReadWrite"
+#     managed_disk_type = "Standard_LRS"
+#   }
+#   os_profile {
+#     computer_name  = "ws${count.index + 1}" #set hostname same as name of VM
+#     # Note: you can't use admin or Administrator in here, Azure won't allow you to do so :-)
+#     admin_username = var.windows_local_admin
+#     #password hardcoded
+#     admin_password = var.windows_local_admin_password
+#   }
+#   #Enable WINRM
+#   os_profile_windows_config {
+#       enable_automatic_upgrades = false
+#       timezone = "Central European Standard Time"
+#       winrm {
+#         protocol = "HTTP"
+#       }
+#   }
+#   tags = {
+#     role = "workstation"
+#   }
+# }
